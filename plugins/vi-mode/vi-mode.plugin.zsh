3e67160614a98ed129f29349eea6b04615d4122a
Jeremy C. Andrus <jeremy_andrus@apple.com>
2014-09-17 10:13:24 -0700

    updates to vi-mode plugin
    Signed-off-by: Jeremy C. Andrus <jeremy_andrus@apple.com>

--- plugins/vi-mode/vi-mode.plugin.zsh
+++ plugins/vi-mode/vi-mode.plugin.zsh
@@ -1,22 +1,19 @@
 function zle-line-init zle-keymap-select {
   zle reset-prompt
 }
+function zle-line-init zle-keymap-select {
+	VIM_PROMPT="%{$fg_bold[yellow]%} [% NORMAL]% %{$reset_color%}"
+	RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/} %{$fg_no_bold[blue]%}[%*]%{$reset_color%}"
+	zle reset-prompt
+}
 
 zle -N zle-line-init
 zle -N zle-keymap-select
 
 bindkey -v
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+bindkey '^w' backward-kill-word
+bindkey '^r' history-incremental-search-backward
 
-# if mode indicator wasn't setup by theme, define default
-if [[ "$MODE_INDICATOR" == "" ]]; then
-  MODE_INDICATOR="%{$fg_bold[red]%}<%{$fg[red]%}<<%{$reset_color%}"
-fi
-
-function vi_mode_prompt_info() {
-  echo "${${KEYMAP/vicmd/$MODE_INDICATOR}/(main|viins)/}"
-}
-
-# define right prompt, if it wasn't defined by a theme
-if [[ "$RPS1" == "" && "$RPROMPT" == "" ]]; then
-  RPS1='$(vi_mode_prompt_info)'
-fi
+export KEYTIMEOUT=1
